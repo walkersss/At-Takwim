21333611d0d6cb048cf71dfca4c0b798bedb599f
Rihab Adib <44679803+walkersss@users.noreply.github.com>
2020-08-15 11:03:07 +0800

    Update HomeScreen.js

--- src/screen/HomeScreen.js
+++ src/screen/HomeScreen.js
@@ -1,28 +1,185 @@
-import * as React from 'react';
-import { Text, View, StyleSheet, Button, Alert, Icon, SafeAreaView } from 'react-native';
-import { IconButton, Colors, Provider as PaperProvider, Card } from 'react-native-paper';
-import {Styles} from './Styles';
-import NewPost from './src/screen/NewPost';
-import { createStackNavigator } from '@react-navigation/stack';
-import { NavigationContainer } from '@react-navigation/native';
-//import AssetExample from './components/AssetExample';
-//import { Router, Scene } from 'react-native-router-flux';
-//import Constants from 'expo-constants';
+import React, { Component } from "react";
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  AsyncStorage,
+  Button,
+  TextInput,
+  Keyboard,
+  Platform, SafeAreaView, Alert
+} from "react-native";
 
-export default function App() {
-  return (
-    <SafeAreaView style={Styles.container}>
-    <View style={Styles.container}>
-      <Text style={Styles.paragraph}>
-        POST TODAY!
-      </Text>
-        <Button title= "Add new Post" onPress={() => this.prop.navigation.navigate('NewPost')} />
-       <IconButton icon='camera' color={Colors.black} size={30} onPress={() => console.log('Pressed')}/>
-      
-    </View>
-    </SafeAreaView>
-    
-  );
+const isAndroid = Platform.OS == "android";
+const viewPadding = 10;
+ 
+export default class ProjectList extends Component {
+  state = {
+    posts: [],
+    text: ""
+  };
+
+  changeTextHandler = text => {
+    this.setState({ text: text });
+  };
+
+  addTask = () => {
+    let notEmpty = this.state.text.trim().length > 0;
+
+    if (notEmpty) {
+      this.setState(
+        prevState => {
+          let { posts, text } = prevState;
+          return {
+            posts: posts.concat({ key: posts.length, text: text }),
+            text: ""
+          };
+        },
+        () => posts.save(this.state.posts)
+      );
+    }
+  };
+
+  deleteTask = i => {
+    this.setState(
+      prevState => {
+        let posts = prevState.posts.slice();
+
+        posts.splice(i, 1);
+
+        return { posts: posts };
+      },
+      () => posts.save(this.state.posts)
+    );
+  };
+  buttonAlert = i =>
+    Alert.alert(
+      "WAIT!",
+      "Sure you want to delete post?",
+      [
+        {
+          text: "Cancel",
+          onPress: () => console.log("Cancel Pressed"),
+          style: "cancel"
+        },
+        { text: "OK", onPress: () => this.deleteTask(i) }
+      ],
+      { cancelable: false });
+  
+  componentDidMount() {
+    Keyboard.addListener(
+      isAndroid ? "keyboardDidShow" : "keyboardWillShow",
+      e => this.setState({ viewMargin: e.endCoordinates.height + viewPadding })
+    );
+
+    Keyboard.addListener(
+      isAndroid ? "keyboardDidHide" : "keyboardWillHide",
+      () => this.setState({ viewMargin: viewPadding })
+    );
+
+    posts.all(posts => this.setState({ posts: posts || [] }));
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+      <View
+        style={[styles.container, { paddingBottom: this.state.viewMargin }]}
+      >
+      <View>
+        <Text style={styles.tajuk}>MAKAN TODAY!</Text>
+        <Text> Let's see what the community have for meal today</Text>
+        </View>
+        <FlatList
+          style={styles.list}
+          data={this.state.posts}
+          renderItem={({ item, index }) =>
+            <View>
+              <View style={styles.listItemCont}>
+                <Text style={styles.listItem}>
+                  {item.text}
+                </Text>
+                <Button title="X" onPress={() => this.buttonAlert(index)} />
+              </View>
+              <View style={styles.hr} />
+            </View>}
+        />
+        <TextInput
+          style={styles.textInput}
+          onChangeText={this.changeTextHandler}
+          onSubmitEditing={this.addTask}
+          value={this.state.text}
+          placeholder="What are you cooking today?"
+          returnKeyType="done"
+          returnKeyLabel="done"
+        />
+      </View></SafeAreaView>
+    );
+  }
 }
+
+let posts = {
+  convertToArrayOfObject(posts, callback) {
+    return callback(
+      posts ? posts.split("||").map((task, i) => ({ key: i, text: task })) : []
+    );
+  },
+  convertToStringWithSeparators(posts) {
+    return posts.map(task => task.text).join("||");
+  },
+  all(callback) {
+    return AsyncStorage.getItem("POSTS", (err, posts) =>
+      this.convertToArrayOfObject(posts, callback)
+    );
+  },
+  save(posts) {
+    AsyncStorage.setItem("POSTS", this.convertToStringWithSeparators(posts));
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: "center",
+    backgroundColor: "#0fc7f5",
+    padding: viewPadding,
+    paddingTop: 20
+  },
+  tajuk: {
+    fontSize: 35,
+    fontWeight: "bold",
+  },
+  list: {
+    width: "100%"
+  },
+  listItem: {
+    paddingTop: 2,
+    paddingBottom: 2,
+    fontSize: 18
+  },
+  hr: {
+    height: 1,
+    backgroundColor: "gray"
+  },
+  listItemCont: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between"
+  },
+  textInput: {
+    height: 40,
+    paddingRight: 10,
+    paddingLeft: 10,
+    borderColor: "gray",
+    borderWidth: isAndroid ? 0 : 1,
+    width: "100%"
+  }
+});
+
+AppRegistry.registerComponent("ProjectList", () => ProjectList);
 
 
